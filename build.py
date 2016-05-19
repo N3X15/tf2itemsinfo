25e18d8a80ea3edd865a665faf4901824c6328cb
Rob Nelson <nexisentertainment@gmail.com>
2016-05-18 19:00:06 -0700

    Fix SPCOMP

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, platform
 from buildtools import os_utils, cmd, log
 
 SOURCEMOD_DIR = os.environ.get('SOURCEMOD18',os.path.join('..','sourcemod','sourcemod','build','package','addons','sourcemod'))
@@ -12,12 +12,16 @@ SPCOMP_FLAGS = [
 	'-i='+os.path.join(PROJECT_DIR,'scripting','include'),
 ]
 def spcomp(filename):
+	SPCOMP='spcomp.exe'
+	if platform.platform() == 'Linux':
+		SPCOMP='spcomp'
+	
 	os_utils.ensureDirExists(PLUGINS_DIR,noisy=True)
 	basename, ext = os.path.splitext(os.path.basename(filename))
 	smxname = basename+'.smx'
 	smxpath = os.path.join(PLUGINS_DIR,smxname)
 	log.info('SPCOMP  '+smxname)
-	cmd([os.path.join(SOURCEMOD_DIR,'scripting','spcomp.exe')]+SPCOMP_FLAGS+['-o='+smxpath,filename], critical=True, show_output=True, echo=False)
+	cmd([os.path.join(SOURCEMOD_DIR,'scripting',SPCOMP)]+SPCOMP_FLAGS+['-o='+smxpath,filename], critical=True, show_output=True, echo=False)
 
 log.info('SOURCEMOD18='+SOURCEMOD_DIR)	
 log.info('TF2ITEMS_DIR='+TF2ITEMS_DIR)	
